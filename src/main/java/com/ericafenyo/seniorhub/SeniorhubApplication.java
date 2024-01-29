a2e9d8995275d4e5685bd8417fec785d6d3a3f42
ericafenyo <ericafenyo@yahoo.com>
2024-01-29 21:22:34 +0100

    Initial commit

---
+++ src/main/java/com/ericafenyo/seniorhub/SeniorhubApplication.java
@@ -0,0 +1,13 @@
+package com.ericafenyo.seniorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericafenyo/seniorhub/SeniorhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericafenyo.seniorhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
