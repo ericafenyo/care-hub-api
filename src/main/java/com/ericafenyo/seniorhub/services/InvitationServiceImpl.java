7cd6850eb05a6bb49a7a8116af4dd6da53b5c61e
ericafenyo <ericafenyo@yahoo.com>
2024-07-04 01:06:13 +0200

    feat: add validation for invitation token

--- src/main/java/com/ericafenyo/seniorhub/services/InvitationServiceImpl.java
+++ src/main/java/com/ericafenyo/seniorhub/services/InvitationServiceImpl.java
@@ -25,24 +25,23 @@
 package com.ericafenyo.seniorhub.services;
 
 import com.ericafenyo.seniorhub.EnvironmentVariables;
+import com.ericafenyo.seniorhub.Messages;
 import com.ericafenyo.seniorhub.dto.AcceptInvitationRequest;
-import com.ericafenyo.seniorhub.dto.InvitationRequest;
 import com.ericafenyo.seniorhub.dto.ValidateInvitationRequest;
-import com.ericafenyo.seniorhub.entities.CaretakerSeniorEntity;
 import com.ericafenyo.seniorhub.entities.InvitationEntity;
+import com.ericafenyo.seniorhub.exceptions.HttpException;
+import com.ericafenyo.seniorhub.exceptions.NotFoundException;
 import com.ericafenyo.seniorhub.exceptions.invitation.InvitationAlreadyUsedException;
-import com.ericafenyo.seniorhub.exceptions.invitation.InvitationException;
 import com.ericafenyo.seniorhub.exceptions.invitation.InvitationExpiredException;
 import com.ericafenyo.seniorhub.exceptions.invitation.InvitationNotFoundException;
-import com.ericafenyo.seniorhub.exceptions.invitation.InviteeNotFoundException;
-import com.ericafenyo.seniorhub.exceptions.invitation.InviterNotFoundException;
-import com.ericafenyo.seniorhub.exceptions.invitation.SeniorNotFoundException;
+import com.ericafenyo.seniorhub.exceptions.role.InvalidRoleException;
 import com.ericafenyo.seniorhub.mapper.InvitationMapper;
 import com.ericafenyo.seniorhub.model.Invitation;
 import com.ericafenyo.seniorhub.model.Mail;
 import com.ericafenyo.seniorhub.model.Report;
-import com.ericafenyo.seniorhub.repository.CaretakerSeniorRepository;
 import com.ericafenyo.seniorhub.repository.InvitationRepository;
+import com.ericafenyo.seniorhub.repository.RoleRepository;
+import com.ericafenyo.seniorhub.repository.TeamRepository;
 import com.ericafenyo.seniorhub.repository.UserRepository;
 import com.ericafenyo.seniorhub.util.Hashing;
 import com.ericafenyo.seniorhub.util.Invitations;
@@ -51,55 +50,76 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.Optional;
+
+import static com.ericafenyo.seniorhub.Messages.ERROR_RESOURCE_NOTFOUND;
+import static com.ericafenyo.seniorhub.Messages.ERROR_RESOURCE_NOTFOUND_CODE;
+import static com.ericafenyo.seniorhub.Messages.MESSAGE_INVITATION_ACCEPTED;
 
 @Service
 @RequiredArgsConstructor
 public class InvitationServiceImpl implements InvitationService {
-    private final CaretakerSeniorRepository caretakerSeniorRepository;
     private final EnvironmentVariables environment;
     private final InvitationMapper invitationMapper;
     private final InvitationRepository invitationRepository;
     private final MailService mailService;
     private final UserRepository userRepository;
+    private final Messages messages;
+    private final TeamRepository teamRepository;
+    private final RoleRepository roleRepository;
 
     @Override
     @Transactional
-    public Report invite(InvitationRequest request) throws InvitationException {
-        // Get the inviter
-        var inviter = userRepository.findById(request.getInviterId());
-        if (inviter.isEmpty()) {
-            throw new InviterNotFoundException();
-        }
+    public Report invite(String teamId, String inviterId, String roleSlug, String email) throws HttpException {
 
-        // Get the senior
-        var senior = userRepository.findById(request.getSeniorId());
-        if (senior.isEmpty()) {
-            throw new SeniorNotFoundException();
-        }
+        // Get the inviter or throw an error if it does not exist
+        var inviter = userRepository.findById(inviterId).orElseThrow(
+            () -> new NotFoundException(
+                messages.format(Messages.ERROR_RESOURCE_WITH_ID_NOTFOUND, "User", inviterId),
+                messages.format(ERROR_RESOURCE_NOTFOUND_CODE, "user")
+            )
+        );
+
+        // Get the team or throw an error if it does not exist
+        var team = teamRepository.findById(teamId).orElseThrow(() -> new NotFoundException(
+            messages.format(Messages.ERROR_RESOURCE_WITH_ID_NOTFOUND, "Team", teamId),
+            messages.format(ERROR_RESOURCE_NOTFOUND_CODE, "team")
+        ));
+
+        var role = roleRepository.findBySlug(roleSlug).orElseThrow(() -> new InvalidRoleException());
 
         String token = Hashing.randomSHA256();
 
         var invitation = new InvitationEntity()
-                .setToken(token)
-                .setEmail(request.getEmail())
-                .setExpiresAt(Instant.now().plusSeconds(environment.getInvitationExpirySeconds()))
-                .setInviter(inviter.get())
-                .setSenior(senior.get());
+            .setToken(token)
+            .setEmail(email)
+            .setRole(role)
+            .setExpiresAt(Instant.now().plusSeconds(environment.getInvitationExpirySeconds()))
+            .setTeam(team)
+            .setInviter(inviter);
 
         invitationRepository.save(invitation);
 
         var baseUrl = environment.getBaseUrl();
-        var link = "%s?token=%s".formatted(baseUrl, token);
+        var link = "%s/invitations?token=%s".formatted(baseUrl, token);
         var context = new Mail.Context();
         context.put("link", link);
-        return mailService.sendInvitation(request.getEmail(), context);
+        return mailService.sendInvitation(email, context);
     }
 
     @Override
-    public Invitation validateInvitation(ValidateInvitationRequest request) throws InvitationException {
+    public Invitation validateInvitation(ValidateInvitationRequest request) throws HttpException {
         var invitation = invitationRepository.findByToken(request.getToken())
-                .orElseThrow(() -> new InvitationNotFoundException());
+            .orElseThrow(() -> new InvitationNotFoundException());
+
+        // Check if the invitation has already been accepted
+        if (invitation.getStatus() == Invitation.Status.ACCEPTED) {
+            throw new InvitationAlreadyUsedException();
+        }
+
+        // Check if the invitation has expired
+        if (Invitations.hasExpired(invitation.getExpiresAt())) {
+            throw new InvitationExpiredException();
+        }
 
         if (Invitations.hasExpired(invitation.getExpiresAt())) {
             throw new InvitationExpiredException();
@@ -114,29 +134,39 @@ public class InvitationServiceImpl implements InvitationService {
 
     @Override
     @Transactional
-    public void acceptInvitation(AcceptInvitationRequest request) throws InvitationException {
+    public Report acceptInvitation(AcceptInvitationRequest request) throws HttpException {
+        // Check if the invitation exists
         var invitation = invitationRepository.findByToken(request.getToken())
-                .orElseThrow(() -> new InvitationNotFoundException());
+            .orElseThrow(() -> new NotFoundException(
+                messages.format(ERROR_RESOURCE_NOTFOUND, "invitation"),
+                messages.format(ERROR_RESOURCE_NOTFOUND_CODE, "invitation")
+            ));
+
+        // Check if the invitation has already been accepted
+        if (invitation.getStatus() == Invitation.Status.ACCEPTED) {
+            throw new InvitationAlreadyUsedException();
+        }
+
+        // Check if the invitation has expired
+        if (Invitations.hasExpired(invitation.getExpiresAt())) {
+            throw new InvitationExpiredException();
+        }
 
         var invitee = userRepository.findByEmail(invitation.getEmail())
-                .orElseThrow(() -> new InviteeNotFoundException());
+            .orElseThrow(() -> new NotFoundException(
+                messages.format(ERROR_RESOURCE_NOTFOUND, "invitee"),
+                messages.format(ERROR_RESOURCE_NOTFOUND_CODE, "invitee")
+            ));
 
-        Optional.ofNullable(invitation.getInviter()).orElseThrow(() -> new InviterNotFoundException());
+        var team = invitation.getTeam();
 
-        var senior = Optional.ofNullable(invitation.getSenior())
-                .orElseThrow(() -> new SeniorNotFoundException());
-
-
-        var caretakerSenior = new CaretakerSeniorEntity()
-                .setCaretaker(invitee)
-                .setSenior(senior);
-
-        // Save the caretakerSenior
-        caretakerSeniorRepository.save(caretakerSenior);
+        team.getMembers().add(invitee);
 
         invitation.setUsedAt(Instant.now());
         invitation.setStatus(Invitation.Status.ACCEPTED);
 
         invitationRepository.save(invitation);
+
+        return new Report(messages.get(MESSAGE_INVITATION_ACCEPTED));
     }
 }
